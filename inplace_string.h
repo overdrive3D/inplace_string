6e63ce1051a9ef1ed07c8d71b42aca37fc915f7f
overdrive3D <overdrive3Dfx@gmail.com>
2025-12-22 22:05:34 +0200

    Explicitly define BufSize constant

--- inplace_string.h
+++ inplace_string.h
@@ -18,7 +18,8 @@ class alignas(16) inplace_string
         "inplace string requires char or wchar_t type");
     static_assert(N >= 15, "inplace string too short");
     static_assert(N < std::numeric_limits<T>::max() - 1, "inplace string too long");
-    static_assert(sizeof(T[N + 1]) >= sizeof(T*) + sizeof(uint32_t),
+    static constexpr size_t BufSize = sizeof(T[N + 1]);
+    static_assert(BufSize > sizeof(T*) + sizeof(uint32_t),
         "sso buffer too small for aliasing");
     template<class, size_t>
     friend class inplace_string;
