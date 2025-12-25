9e12d1dc40728f2de426b1f4f021d477323c8890
overdrive3D <overdrive3Dfx@gmail.com>
2025-12-25 14:08:08 +0200

    Implemented literal_string class that computes string hash in compile-time

---
+++ literal_string.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <stdint.h>
+#include "fnv_hash.h"
+
+template<class T>
+class literal_string
+{
+public:
+    template<size_t N>
+    constexpr literal_string(const T (&str)[N]):
+        str(str), len(N), uid(fnv1(str)) {}
+    constexpr const T* c_str() const { return str; }
+    constexpr size_t length() const { return len; }
+    constexpr uint32_t hash() const { return uid; }
+
+private:
+    const T *const str;
+    const size_t len;
+    const uint32_t uid;
+};
+
+#define WIDEN2(x) L##x
+#define WIDEN(x) WIDEN2(x)
+
+#define Literal(str)\
+([]() constexpr {\
+    constexpr literal_string<char> lit(str);\
+    return lit;\
+}())
+
+#define WLiteral(str)\
+([]() constexpr {\
+    constexpr literal_string<wchar_t> lit(WIDEN(str));\
+    return lit;\
+}())
